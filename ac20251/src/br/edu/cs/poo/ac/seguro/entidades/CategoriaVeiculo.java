02a905c8bab65789437262f8666b63456e0c176b
Luiz Felipe <luiz.nogplus@gmail.com>
2025-04-22 21:41:33 -0300

    teste de commit

--- ac20251/src/br/edu/cs/poo/ac/seguro/entidades/CategoriaVeiculo.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/entidades/CategoriaVeiculo.java
@@ -1,5 +1,45 @@
 package br.edu.cs.poo.ac.seguro.entidades;
-
-public class CategoriaVeiculo {
-
+import static br.edu.cs.poo.ac.seguro.entidades.PrecosAnosCategoria.*;
+public enum CategoriaVeiculo {
+	
+	
+	BASICO(1,"Veículo econômico", PA_BASICO),
+	INTERMEDIARIO(2,"Veículo de categoria média", PA_INTERMEDIARIO),
+	LUXO(3, "Veículo de luxo", PA_LUXO),
+	SUPER_LUXO(4, "Veículo exclusivo", PA_SUPER_LUXO),
+	ESPORTIVO(5, "Veículo esportivo", PA_ESPORTIVO);
+		
+	private int codigo;
+	private String nome;
+	private PrecoAno[] precosAnos;
+	
+	private CategoriaVeiculo(int codigo, String nome, PrecoAno[] precosAnos) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.precosAnos = precosAnos;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setPrecosAnos(PrecoAno[] precosAnos) {
+		this.precosAnos = precosAnos;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public PrecoAno[] getPrecosAnos() {
+		return precosAnos;
+	}
 }
--- ac20251/src/br/edu/cs/poo/ac/seguro/entidades/TipoSinistro.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/entidades/TipoSinistro.java
@@ -1,5 +1,12 @@
 package br.edu.cs.poo.ac.seguro.entidades;
 
 public enum TipoSinistro {
-
+	COLISAO(1,"Colisão"),
+	INCENDIO(2,"Incêndio"),
+	FURTO(3, "Furto"),
+	ENCHENTE(4, "Enchente"),
+	DEPREDACAO(5, "Depredação");
 }
+
+private enum TipoSinistro();
+
