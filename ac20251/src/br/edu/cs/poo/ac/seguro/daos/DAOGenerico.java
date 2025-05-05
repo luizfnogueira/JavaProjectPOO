c64cc02a7cdfc523838a868e273b82293ce7fadc
Luiz Felipe <luiz.nogplus@gmail.com>
2025-05-05 12:49:30 -0300

    gitignore updated

--- ac20251/src/br/edu/cs/poo/ac/seguro/daos/DAOGenerico.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/daos/DAOGenerico.java
@@ -1,5 +1,7 @@
 package br.edu.cs.poo.ac.seguro.daos;
 
-public class DAOGenerico {
+import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 
+public class DAOGenerico {
+	protected CadastroObjetos cadastro;
 }
