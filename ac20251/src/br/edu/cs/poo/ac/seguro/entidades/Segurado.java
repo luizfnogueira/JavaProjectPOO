d1120555922d65f9c80534930739581294af45dc
Luiz Felipe <luiz.nogplus@gmail.com>
2025-05-05 21:58:01 -0300

    ajuste de erros

--- ac20251/src/br/edu/cs/poo/ac/seguro/entidades/Segurado.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/entidades/Segurado.java
@@ -1,73 +1,67 @@
 package br.edu.cs.poo.ac.seguro.entidades;
-import java.time.LocalDate;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.Period;
 
-public class Segurado {
+public class Segurado implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
 	private String nome;
 	private Endereco endereco;
 	private LocalDate dataCriacao;
 	private BigDecimal bonus;
-	
-	public Segurado(String nome, Endereco endereco, LocalDate dataCriacao, BigDecimal bonus) {
-		super();
-		this.nome = nome;
-		this.endereco = endereco;
-		this.dataCriacao = dataCriacao;
-		this.bonus = bonus;
-	}
 
 	public String getNome() {
 		return nome;
 	}
-
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	
 	public Endereco getEndereco() {
 		return endereco;
 	}
-
+	
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
-
+	
 	protected LocalDate getDataCriacao() {
 		return dataCriacao;
 	}
-
-	protected void setDataCriacao(LocalDate dataCriacao) {
-		this.dataCriacao = dataCriacao;
+	
+	protected void setDataCriacao(LocalDate data) {
+		this.dataCriacao = data;
 	}
-
+	
 	public BigDecimal getBonus() {
 		return bonus;
 	}
-
-	public void setBonus(BigDecimal bonus) {
+	
+	public Segurado(String nome, Endereco endereco, LocalDate dataCriacao, BigDecimal bonus) {
+		this.nome = nome;
+		this.endereco = endereco;
+		this.dataCriacao = dataCriacao;
 		this.bonus = bonus;
 	}
 	
 	public int getIdade() {
-        LocalDate hoje = LocalDate.now();
-        if (dataCriacao != null) {
-            return Period.between(dataCriacao, hoje).getYears();
-        } else {
-            return 0;
-        }
-    }
+		LocalDate agora = LocalDate.now();
+		int idade = Period.between(getDataCriacao(), agora).getYears();
+		return idade;
+	}
 	
 	public void creditarBonus(BigDecimal valor) {
-        if (valor != null) {
-            this.bonus = this.bonus.add(valor);
-        }
-    }
+		bonus = bonus.add(valor);	
+	}
 	
 	public void debitarBonus(BigDecimal valor) {
-        if (valor != null) {
-            this.bonus = this.bonus.subtract(valor);
-        }
-    }
-	
+		if (bonus.compareTo(valor) >= 0) {
+			bonus = bonus.subtract(valor);
+		}
+	}
 }
