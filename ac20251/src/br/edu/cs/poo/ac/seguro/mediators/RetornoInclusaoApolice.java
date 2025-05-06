270f6c099d95f6bf19b723a5b1af62d7af8a0f18
Luiz Felipe <luiz.nogplus@gmail.com>
2025-05-05 21:17:03 -0300

    Mediators pt2

--- ac20251/src/br/edu/cs/poo/ac/seguro/mediators/RetornoInclusaoApolice.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/mediators/RetornoInclusaoApolice.java
@@ -1,5 +1,22 @@
 package br.edu.cs.poo.ac.seguro.mediators;
 
-public class RetornoInclusaoApolice {
+import lombok.Getter;
 
+@Getter
+public class RetornoInclusaoApolice {
+	private String numeroApolice;
+	private String mensagemErro;
+	public RetornoInclusaoApolice(String numeroApolice, String mensagemErro) {
+		if (numeroApolice == null && mensagemErro == null) {
+			throw new RuntimeException(
+				"Número da apólice e mensagem de erro não podem ser ambas nulas");
+		}
+		if (numeroApolice != null && mensagemErro != null) {
+			throw new RuntimeException(
+				"Número da apólice e mensagem de erro não podem ser ambas preenchidas");
+		}
+		this.numeroApolice = numeroApolice;
+		this.mensagemErro = mensagemErro;
+
+	}
 }
